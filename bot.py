39f11fc1eeeb89e525a2e9f9c7710cbf667552f5
Kirill1922363 <kirillhalenko1210@gmail.com>
2025-06-12 12:23:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,10 +9,10 @@ from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, BotCommand
 from app.keyboards import math_menu_keyboard
 from settings import TOKEN
-from app.handlers import router  
+from app.handlers import router
+
 dp = Dispatcher()
 dp.include_router(router)
-
 
 
 @dp.message(CommandStart())
@@ -20,29 +20,26 @@ async def command_start_handler(message: Message) -> None:
     await message.answer(
         f"Привіт, {message.from_user.full_name}!", reply_markup=math_menu_keyboard()
     )
-@dp.message(Command("stop"))
-async def command_start_handler(message: Message) -> None:
-    await message.answer(
-        f"Бот зупинено, {message.from_user.full_name}!", reply_markup=math_menu_keyboard()
-    )
+
+
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
-    await bot.set_my_commands([
-        BotCommand(command="start", description="Запустити бота"),
-        BotCommand(command="stop", description="Запустити бота"),
-        BotCommand(command="calculator", description="Обчислити вираз"),
-        BotCommand(command="geometric_functions", description="Геометричні функції"),
-        BotCommand(command="help", description="Довідка"),
-        
-    ])
+    await bot.set_my_commands(
+        [
+            BotCommand(command="start", description="Запустити бота"),
+            BotCommand(command="calculator", description="Обчислити вираз"),
+            BotCommand(command="geometric_calculator", description="Геометричний калькулятор"),
+            BotCommand(command="help", description="Довідка"),
+        ]
+    )
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
-
 
 
