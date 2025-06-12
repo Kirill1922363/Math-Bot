04fa0c7794f66001423df550f30b687819918d74
Kirill1922363 <kirillhalenko1210@gmail.com>
2025-06-12 12:21:42 +0300

    Update keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,24 +1,28 @@
-from settings import PAGE_SIZE
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from aiogram.filters.callback_data import CallbackData
 from aiogram.types.inline_keyboard_button import InlineKeyboardButton
+from settings import PAGE_SIZE
+
 BUTTON_CALCULATE = " Обчислити вираз"
 GEOMETRIC_CALCULATOR = " Геометричний калькулятор"
-STOP = "Стоп"
+
+
 def math_menu_keyboard():
     builder = ReplyKeyboardBuilder()
     builder.button(text=GEOMETRIC_CALCULATOR)
     builder.button(text=BUTTON_CALCULATE)
-    builder.button(text=STOP)
-    builder.adjust(2, 1) 
+    builder.adjust(2)
     return builder.as_markup(resize_keyboard=True)
+
 
 def menu_keyboards():
     return math_menu_keyboard()
 
+
 class TaskCallback(CallbackData, prefix="task", sep=";"):
     id: int
     title: str
+
 
 def tasks_keyboard_markup(tasks_list: list[dict], page: int = 1):
     builder = InlineKeyboardBuilder()
@@ -28,18 +32,19 @@ def tasks_keyboard_markup(tasks_list: list[dict], page: int = 1):
 
     for index, task in enumerate(tasks_list[start_idx:end_idx], start=start_idx):
         callback_task = TaskCallback(id=index, title=task["title"])
-        builder.button(
-            text=task["title"],
-            callback_data=callback_task.pack()
-        )
+        builder.button(text=task["title"], callback_data=callback_task.pack())
 
-    builder.adjust(1)  
+    builder.adjust(1)
     nav_buttons = []
     if page > 1:
-        nav_buttons.append(InlineKeyboardButton(text='< назад', callback_data=f"page_{page-1}"))
+        nav_buttons.append(
+            InlineKeyboardButton(text="< назад", callback_data=f"page_{page-1}")
+        )
     if page < total_pages:
-        nav_buttons.append(InlineKeyboardButton(text='вперед >', callback_data=f"page_{page+1}"))
+        nav_buttons.append(
+            InlineKeyboardButton(text="вперед >", callback_data=f"page_{page+1}")
+        )
     if nav_buttons:
         builder.row(*nav_buttons)
-    
+
     return builder.as_markup()
